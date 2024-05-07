ff5a7d3a2e250c7f8e5146a576e5a4164d4cf4ff
Tazem1 <74832635+Tazem1@users.noreply.github.com>
2024-05-07 09:53:42 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,3 +2,16 @@ var Engine = Matter.Engine,
   World = Matter.World,
   Events = Matter.Events,
   Bodies = Matter.Bodies;
+
+function setup(){
+  createCanvas(800,800)
+  engine = Engine.create()
+  world = engine.world
+  ground = new Ground(width/2,height,width,20)
+}
+
+function draw(){
+  background("grey")
+  Engine.update(engine)
+  ground.display()
+}
